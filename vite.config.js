0abfa4d7dd52ef01d72ab0dc64e9363eb684d8f8
profWilliamArte <arteagawilliam13@hotmail.com>
2023-11-01 13:08:27 -0400

    configura config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-  base: "https://profWilliamArte.github.io/htmlcssvite01",
-})
+    base: "https://profWilliamArte.github.io/htmlcssvite01",
+});
