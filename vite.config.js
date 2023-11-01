3ed8b32ea53300424f14fe1446dcc7cc9f23aea1
profWilliamArte <arteagawilliam13@hotmail.com>
2023-11-01 13:46:15 -0400

    otro

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
+// vite.config.js
+export default {
+    build: {
+      rollupOptions: {
+        input: {
+          main: './src/index.html',
+          accion: './src/accion.html',
+          animacion: './src/animacion.html',
+          aventura: './src/aventura.html',
+          comedia: './src/comedia.html'
+        }
+      }
+    }
+}  
 
+
+/*
 import { defineConfig } from 'vite';
+
+
 
 export default defineConfig({
     base: "https://profWilliamArte.github.io/htmlcssvite01",
+    
 });
+
+*/
+
