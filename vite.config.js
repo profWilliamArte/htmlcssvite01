0a605be6c51725950e116ba75bb82638a07b13fd
profWilliamArte <arteagawilliam13@hotmail.com>
2023-11-03 18:29:11 -0400

    ultimo

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,8 @@
-// vite.config.js
-/*
-export default {
-    build: {
-      rollupOptions: {
-        input: {
-          index: 'index.html',
-          accion: 'accion.html',
-          animacion: 'animacion.html',
-          aventura: 'aventura.html',
-          comedia: 'comedia.html'
-        }
-      }
-    }
-}  
 
-
-*/
 import { defineConfig } from 'vite';
 export default defineConfig({
     base: "https://profWilliamArte.github.io/htmlcssvite01",
-    server: {
-        port: "http://localhost:4173/htmlcssvite01", // Puerto en el que se ejecutará el servidor local
-      },
+
     build: {
         rollupOptions: {
           input: {
